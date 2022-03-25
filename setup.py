5c19178a5d1839dba013c0b13621dc0ad9671fd9
Felix.Vorlaender <felix.vorlaender@mittelstand.ai>
2022-03-25 11:04:19 +0100

    create autostart link on install

--- setup.py
+++ setup.py
@@ -1,16 +1,64 @@
 import sys
 from cx_Freeze import setup, Executable
 
-# Dependencies are automatically detected, but it might need fine tuning.
-# "packages": ["os"] is used as example only
+# http://msdn.microsoft.com/en-us/library/windows/desktop/aa371847(v=vs.85).aspx
+shortcut_table = [
+    ("DesktopShortcut",        # Shortcut
+     "DesktopFolder",          # Directory_
+     "Pytabber",               # Name
+     "TARGETDIR",              # Component_
+     "[TARGETDIR]pytabber.exe",# Target
+     None,                     # Arguments
+     None,                     # Description
+     None,                     # Hotkey
+     None,                     # Icon
+     None,                     # IconIndex
+     None,                     # ShowCmd
+     'TARGETDIR'               # WkDir
+     ),
+
+    ("Start Menu",             # Shortcut
+     "StartMenuFolder",        # Directory_
+     "Pytabber",              # Name
+     "TARGETDIR",              # Component_
+     "[TARGETDIR]pytabber.exe",# Target
+     None,                     # Arguments
+     None,                     # Description
+     None,                     # Hotkey
+     None,                     # Icon
+     None,                     # IconIndex
+     None,                     # ShowCmd
+     'TARGETDIR'               # WkDir
+     ),
+
+    ("Run On Windows Startup", # Shortcut
+     "StartupFolder",        # Directory_
+     "AutoStart Pytabber",         # Name
+     "TARGETDIR",              # Component_
+     "[TARGETDIR]pytabber.exe",# Target
+     None,                     # Arguments
+     None,                     # Description
+     None,                     # Hotkey
+     None,                     # Icon
+     None,                     # IconIndex
+     None,                     # ShowCmd
+     'TARGETDIR'               # WkDir
+     )
+]
+
+
 options = {"build_exe":
                 {
                     # "packages": ["win32gui", "win32com.client", "keyboard", "threading", "pystray", "screeninfo", "PIL"],
                     "include_files":"./tab-key.ico" ,
                     "optimize": 2
+                },
+            "bdist_msi":{
+                    "data":{
+                        "Shortcut": shortcut_table
+                    }
                 }
             }
-
 
 setup(
     name="Pytabber",
@@ -20,9 +68,7 @@ setup(
     executables=[
         Executable("pytabber.py", 
             base="Win32GUI",
-            icon=r".\tab-key.ico",
-            shortcut_name="PyTabber",
-            shortcut_dir="DesktopFolder"
+            icon=r".\tab-key.ico"
             )
         ],
 )
